4138c79a40810cd766bd4b2db1b0db4894cf4791
dvirGev <dvir87879@gmail.com>
2021-11-24 15:24:56 +0200

    first commit

---
+++ src/MyDirectedWeightedGraph.java
@@ -0,0 +1,93 @@
+import java.io.FileReader;
+import java.util.Iterator;
+
+import api.DirectedWeightedGraph;
+import api.EdgeData;
+import api.NodeData;
+
+import com.google.gson.*;
+
+public class MyDirectedWeightedGraph implements DirectedWeightedGraph {
+    public MyDirectedWeightedGraph(String jsonFile) {
+        try {
+            FileReader fr = new FileReader(jsonFile);
+            GsonBuilder gb = new GsonBuilder();
+        }
+        catch (Exception e) {
+            
+        }
+    }
+
+    @Override
+    public NodeData getNode(int key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public EdgeData getEdge(int src, int dest) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addNode(NodeData n) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void connect(int src, int dest, double w) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Iterator<NodeData> nodeIter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<EdgeData> edgeIter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<EdgeData> edgeIter(int node_id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public NodeData removeNode(int key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public EdgeData removeEdge(int src, int dest) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int nodeSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int edgeSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getMC() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
