c6c64ad8f84f3b6c3e9008482399e7d29283032a
dvirBiton1 <dvir199710@gmail.com>
2021-11-27 20:57:51 +0200

    dvir biton

---
+++ src/MyEdgeData.java
@@ -0,0 +1,93 @@
+import api.EdgeData;
+
+public class MyEdgeData implements EdgeData {
+    int src;
+    int dest;
+    double weight;
+    String info;
+    int tag;
+
+    public MyEdgeData(int src, int dest, double weight, String info, int tag){
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+        this.info = info;
+        this.tag = tag;
+
+    }
+    /**
+     * The id of the source node of this edge.
+     * @return
+     */
+    @Override
+    public int getSrc() {
+        return this.src;
+    }
+
+    /**
+     * The id of the destination node of this edge
+     * @return
+     */
+    @Override
+    public int getDest() {
+        return this.dest;
+    }
+
+    /**
+     * @return the weight of this edge (positive value).
+     */
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    /**
+     * Returns the remark (meta data) associated with this edge.
+     * @return
+     */
+    @Override
+    public String getInfo() {
+        return this.info;
+    }
+
+    /**
+     * Allows changing the remark (meta data) associated with this edge.
+     * @param s
+     */
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+
+    }
+
+    /**
+     * Temporal data (aka color: e,g, white, gray, black)
+     * which can be used be algorithms
+     * @return
+     */
+    @Override
+    public int getTag() {
+        return tag;
+    }
+
+    /**
+     * This method allows setting the "tag" value for temporal marking an edge - common
+     * practice for marking by algorithms.
+     * @param t - the new value of the tag
+     */
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
+
+    @Override
+    public String toString() {
+        return "MyEdgeData{" +
+                "src=" + src +
+                ", dest=" + dest +
+                ", weight=" + weight +
+                ", info='" + info + '\'' +
+                ", tag=" + tag +
+                '}';
+    }
+}
