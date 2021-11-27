eb99e7386073399becdfffa1701e95f18f54a99a
avgilboa <you@example.comgilboa.aviad@gmail.com>
2021-11-27 21:22:02 +0200

    reading the json files

--- src/MyEdgeData.java
+++ src/MyEdgeData.java
@@ -12,7 +12,7 @@ public class MyEdgeData implements EdgeData {
         this.dest = dest;
         this.weight = weight;
         this.info = null;
-        this.tag = null;
+        this.tag = Integer.parseInt(null);
 
     }
     /**
