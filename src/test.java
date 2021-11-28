ead7103664162674aa6c0feb822f9ee93e154702
dvirGev <dvir87879@gmail.com>
2021-11-28 10:32:38 +0200

    commit

--- src/test.java
+++ src/test.java
@@ -1,18 +1,18 @@
-import java.util.HashMap;
-import java.util.Vector;
+// Java program to traverse through
+// a hashmap using iterator
 
-public class test {
-    public static void main(String[] args) {
-        HashMap <Vector<Integer>, Integer> edges = new HashMap<>();
-        Vector<Integer> key = new Vector<>();
-        key.add(2);
-        key.add(5);
-        System.out.println(key);
-        edges.put(key, 5);
-        Vector<Integer> key2 = new Vector<>();
-        key2.add(5);
-        key2.add(2);
-        System.out.println(edges.get(key2));
+import java.util.*;
 
-    }
+class GfG {
+	public static void main(String[] args)
+	{
+		// Consider the hashmap contains
+		// student name and their marks
+		HashMap<Integer, MyEdgeData> hm = new HashMap<Integer, MyEdgeData>();
+        hm.put(0, new MyEdgeData(1, 2, 0));
+        hm.put(1, new MyEdgeData(1, 3, 0));
+        hm.put(2, new MyEdgeData(1, 4, 0));
+        hm.values().iterator();
+		Iterator<Map.Entry<Vector<Integer,MyEdgeData>> iter = hm.entrySet().iterator();
+	}
 }
