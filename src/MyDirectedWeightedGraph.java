1180042c920f7202f0da36c8fc477461e76f05ea
dvirGev <dvir87879@gmail.com>
2021-12-02 17:15:04 +0200

    adddddddd

--- src/MyDirectedWeightedGraph.java
+++ src/MyDirectedWeightedGraph.java
@@ -12,11 +12,13 @@ import api.NodeData;
 public class MyDirectedWeightedGraph implements DirectedWeightedGraph{
     private HashMap <Integer, NodeData> nodes;
     private HashMap <Vector<Integer>,  EdgeData> edges;
+    private HashMap<Integer, Degree> edgeByNode;
     private int mc;
 
     public MyDirectedWeightedGraph() {
         nodes = new HashMap<>();
         edges = new HashMap<>();
+        edgeByNode = new HashMap<>();
         mc = 0;
     }
     public HashMap<Integer, NodeData> getNodes() {
@@ -39,13 +41,11 @@ public class MyDirectedWeightedGraph implements DirectedWeightedGraph{
 
     @Override
     public void addNode(NodeData n) {
-        if(n.getClass() !=  MyNodeData.class) {
-            throw new RuntimeException("the NodeData class error");
-        }
         if (nodes.get(n.getKey()) != null) {
             throw new RuntimeException("the Node key already exists");
         }
         nodes.put(n.getKey(), (MyNodeData)n);
+        edgeByNode.put(n.getKey(), new Degree());
         ++mc;
     }
 
@@ -53,12 +53,10 @@ public class MyDirectedWeightedGraph implements DirectedWeightedGraph{
     public void connect(int src, int dest, double w) {
         MyEdgeData edge = new MyEdgeData(src, dest, w);
         Vector<Integer> vector = buildVector(src, dest);
-        edges.remove(vector);
+        removeEdge(src, dest);
         edges.put(vector, edge);
-        MyNodeData node = (MyNodeData) nodes.get(src);
-        node.addFromeMe(dest, edge);
-        node = (MyNodeData) nodes.get(dest);
-        node.addToMe(src, edge);
+        edgeByNode.get(src).fromMe.put(dest, edge);
+        edgeByNode.get(dest).toMe.put(src, edge);
         ++mc;
     }
 
@@ -74,39 +72,38 @@ public class MyDirectedWeightedGraph implements DirectedWeightedGraph{
 
     @Override
     public Iterator<EdgeData> edgeIter(int node_id) {
-        MyNodeData node = (MyNodeData) nodes.get(node_id);
-        return node.getFromMeIterator();
+        return edgeByNode.get(node_id).fromMe.values().iterator();
     }
 
     @Override
     public NodeData removeNode(int key) {
-        MyNodeData node = (MyNodeData) nodes.remove(key);
-        Iterator<EdgeData> iter = node.getFromMeIterator();
+        ++mc;
+        NodeData node =  nodes.remove(key);
+        if (node == null) {
+            return null;
+        }
+        Iterator<EdgeData> iter = edgeByNode.get(key).fromMe.values().iterator();
         while(iter.hasNext()){
             EdgeData edge = iter.next();
-            Vector<Integer> vector = buildVector(edge.getSrc(), edge.getDest());
-            edges.remove(vector);
-            MyNodeData dest = (MyNodeData) nodes.get(edge.getDest());
-            dest.removeToMe(edge.getDest());
+            edges.remove(edge);
+            edgeByNode.get(edge.getDest()).toMe.remove(key);
         }
-        iter = node.getToMeIterator();
+        iter = edgeByNode.get(key).toMe.values().iterator();
         while(iter.hasNext()){
             EdgeData edge = iter.next();
-            Vector<Integer> vector = buildVector(edge.getSrc(), edge.getDest());
-            edges.remove(vector);
-            MyNodeData dest = (MyNodeData) nodes.get(edge.getSrc());
-            dest.removeToMe(edge.getSrc());
+            edges.remove(edge);
+            edgeByNode.get(key).fromMe.remove(edge.getDest());
         }
+        edgeByNode.remove(key);
         return node;
     }
 
     @Override
     public EdgeData removeEdge(int src, int dest) {
         Vector<Integer> vector = buildVector(src, dest);
-        MyNodeData node = (MyNodeData) nodes.get(src);
-        node.removeFromMe(dest);
-        node = (MyNodeData) nodes.get(dest);
-        node.removeToMe(src);
+        edges.remove(vector);
+        edgeByNode.get(src).fromMe.remove(dest);
+        edgeByNode.get(dest).toMe.remove(src);
         ++mc;
         return edges.remove(vector);
     }
@@ -132,4 +129,17 @@ public class MyDirectedWeightedGraph implements DirectedWeightedGraph{
         vector.add(dest);
         return vector;
     }
+    
+    private void stam(){
+        Degree temp = new Degree();
+        
+    }
+    private class Degree {
+        public HashMap <Integer,  EdgeData> fromMe;
+        public HashMap <Integer,  EdgeData> toMe;
+        public Degree() {
+            this.fromMe = new HashMap<>();
+            this.toMe = new HashMap<>();
+        }
+    } 
 }
