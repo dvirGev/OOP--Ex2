95ff34566fa2ce5503d67a573e85451baaa0f46b
dvirGev <dvir87879@gmail.com>
2021-12-05 20:22:36 +0200

    fix main

--- src/gui/MyFrame.java
+++ src/gui/MyFrame.java
@@ -1,19 +1,33 @@
 package gui;
 import api.DirectedWeightedGraph;
 //import api.NodeData;
+import api.DirectedWeightedGraphAlgorithms;
 
 import javax.swing.*;
-
+import java.awt.event.*;    
+import java.io.*; 
 import java.awt.*;
 
-public class MyFrame extends JFrame{
-                
+public class MyFrame extends JFrame implements ActionListener{
+    private JMenuBar mb;    
+    private JMenu grapgOp;    
+    private JMenuItem nodeSize; 
 
-    public MyFrame(DirectedWeightedGraph ans){
+    DirectedWeightedGraph graph;
+
+    public MyFrame(DirectedWeightedGraphAlgorithms ans){
         super();
-        //this.add(new PanelelForMe());
-        MyPanel myPanel = new MyPanel(ans);
-//        this.setSize(500,500);
+        graph = ans.getGraph();
+        nodeSize=new JMenuItem("Size Of Nodes");    
+        nodeSize.addActionListener(this);         
+        grapgOp=new JMenu("Graph");    
+        grapgOp.add(nodeSize);             
+        mb=new JMenuBar();  
+        mb.setBounds(0, 0, 50, 20);
+        
+        mb.add(grapgOp);                         
+        setJMenuBar(mb);
+        MyPanel myPanel = new MyPanel(ans.getGraph());
         this.setLayout(new BorderLayout());
         
 
@@ -25,5 +39,13 @@ public class MyFrame extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == nodeSize) {
+            String message = "The Size Of The Nodes In The Graph is: " + graph.nodeSize();
+            JOptionPane.showMessageDialog(new JFrame(), message, "Size Of Nodes", JOptionPane.DEFAULT_OPTION);
+        }
+        
     }
 }
